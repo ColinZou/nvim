cd69c13642703364b19920e191050816c7c35ae0
Colin Zou <colin.zou@solera.com>
2023-12-05 11:44:25 +0800

    no more bomb problem

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = {
+    "*.yaml",
+    "*.yml",
+    "yaml",
+    "*.js",
+    "*.json",
+    "json",
+    "*.md",
+    "markdown",
+    "*.text",
+    "text",
+  },
+  callback = function()
+    vim.cmd("set nobomb")
+  end,
+})
