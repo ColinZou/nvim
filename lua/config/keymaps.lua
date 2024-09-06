d48210022efae2a0c89b01dfd16ab9ed7c02e5be
Colin Zou <colin.zou@solera.com>
2024-09-06 11:12:06 +0800

    chore: allowing autoformat customization

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.del("n","<leader>cf")
-vim.keymap.set("v", "<leader>cf", vim.lsp.buf.format, { silent = true, noremap = true })
+if vim.g.autoformat == false then
+  vim.keymap.del("n", "<leader>cf")
+  vim.keymap.set("v", "<leader>cf", vim.lsp.buf.format, { silent = true, noremap = true })
+end
