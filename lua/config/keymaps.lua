9b845c37990f6230db6e5cae80cfa5c29ec05490
Colin Zou <colin.zou@solera.com>
2024-09-07 07:48:31 +0800

    chore: removing lsp config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 if vim.g.autoformat == false then
-  vim.keymap.del("n", "<leader>cf")
+  vim.keymap.del("v", "<leader>cf")
   vim.keymap.set("v", "<leader>cf", vim.lsp.buf.format, { silent = true, noremap = true })
 end
