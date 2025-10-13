c888adbe196e39ee66786552e2cd840c0118f19c
Zou Colin <colin.zou@solera.com>
2025-10-13 21:26:50 +0800

    chore: local old version

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -65,11 +65,36 @@ lspconfig.helm_ls.setup({
 lspconfig.yamlls.setup({
   capabilities = lsp_capabilities,
 })
+
 vim.lsp.config("roslyn", {
   capabilities = lsp_capabilities,
   settings = {
-    -- This may not reliably set the C# language version, as it's
-    -- typically controlled by project files.
-    ["csharp|langversion"] = "12.0",
+    ["csharp"] = {
+      language_version = "9.0",
+      lang_version = "9.0",
+    },
+    ["csharp|code_lens"] = {
+      dotnet_enable_references_code_lens = true,
+      dotnet_enable_tests_code_lens = true,
+    },
+    ["csharp|completion"] = {
+      dotnet_show_name_completion_suggestions = true,
+      dotnet_show_completion_items_from_unimported_namespaces = true,
+      dotnet_provide_regex_completions = true,
+    },
+    ["csharp|inlay_hints"] = {
+      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+      csharp_enable_inlay_hints_for_types = true,
+      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+      dotnet_enable_inlay_hints_for_literal_parameters = true,
+      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+      dotnet_enable_inlay_hints_for_other_parameters = true,
+      dotnet_enable_inlay_hints_for_parameters = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+    },
   },
 })
--- lua/plugins/rosyln.lua
+++ lua/plugins/rosyln.lua
@@ -1,7 +1,25 @@
 return {
   "seblyng/roslyn.nvim",
-  ft = "cs",
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
     filewatching = "roslyn",
+    broad_search = true,
   },
+  init = function()
+    vim.keymap.set("n", "<leader>ds", function()
+      if not vim.g.roslyn_nvim_selected_solution then
+        return vim.notify("No solution file found")
+      end
+
+      local projects = require("roslyn.sln.api").projects(vim.g.roslyn_nvim_selected_solution)
+      local files = vim
+        .iter(projects)
+        :map(function(it)
+          return vim.fs.dirname(it)
+        end)
+        :totable()
+
+      Snacks.picker.files({ dirs = files })
+    end)
+  end,
 }
